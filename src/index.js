2ef859707f3455866ddfa3e39b334381b20adbd0
Farès <faresboukhezna@gmail.com>
2020-11-14 02:16:13 +0100

    fix(Pagination): change pagination to be dynamic

--- src/index.js
+++ src/index.js
@@ -12,10 +12,20 @@ const apiUri = 'https://opendata.paris.fr/api/records/1.0/search/?dataset=que-fa
 const GetEvents = (q, start, rows) => axios.get(apiUri, { params: { q, start, rows } })
   .then((res) => res.data);
 
-const GeneratePages = (nbMax, rows) => {
+/* const GeneratePages = (nbMax, rows, goToPage) => {
   const pages = [];
   for (let i = 1; i <= nbMax / rows; i += 1) {
-    pages.push(<Pagination.Item key={i}>{i}</Pagination.Item>);
+    pages.push(<Pagination.Item key={i} onClick={() => goToPage(i)}>{i}</Pagination.Item>);
+  }
+  return pages;
+}; */
+
+const GenerateCurrentPaginationArray = (currentPage, maxPages) => {
+  const pages = [];
+  for (let i = currentPage - 2; i <= currentPage + 2; i += 1) {
+    if (i > 0 && i <= maxPages) {
+      pages.push(i);
+    }
   }
   return pages;
 };
@@ -27,26 +37,45 @@ class App extends React.Component {
       response: [],
       rows: 10,
       start: 0,
-      pages: [],
+      query: '',
+      currentActivePage: 1,
+      maxPages: 1,
     };
 
     this.handleSearch = this.handleSearch.bind(this);
+    this.goToPage = this.goToPage.bind(this);
   }
 
   componentDidMount() {
     const { rows } = this.state;
 
-    GetEvents('', 0, rows).then((res) => { this.setState({ response: res, pages: GeneratePages(res.nhits, rows) }); });
+    GetEvents('', 0, rows).then((res) => { this.setState({ response: res, maxPages: res.nhits / rows }); });
   }
 
   handleSearch(query) {
     const { rows, start } = this.state;
 
-    GetEvents(query, start, rows).then((res) => { this.setState({ response: res, start: 0 }); });
+    GetEvents(query, start, rows)
+      .then((res) => {
+        this.setState({
+          response: res, start: 0, query, maxPages: res.nhits / rows, currentActivePage: 1,
+        });
+      });
+  }
+
+  goToPage(number) {
+    const { rows, query } = this.state;
+
+    GetEvents(query, number * rows, rows)
+      .then((res) => {
+        this.setState({
+          response: res, start: number * rows, query, currentActivePage: number,
+        });
+      });
   }
 
   render() {
-    const { response, pages } = this.state;
+    const { response, currentActivePage, maxPages } = this.state;
 
     return (
       <Container fluid>
@@ -56,8 +85,25 @@ class App extends React.Component {
             {response.records && response.records.map((event, index) => (
               <EventList event={event.fields} index={index} key={event.recordid} />))}
           </Accordion>
+          <Pagination>
+            <Pagination.First />
+            <Pagination.Prev />
+            <Pagination.Ellipsis />
+            {GenerateCurrentPaginationArray(currentActivePage, maxPages)
+              .map((i) => (
+                <Pagination.Item
+                  key={i}
+                  active={currentActivePage === i}
+                  onClick={() => { this.goToPage(i); }}
+                >
+                  {i}
+                </Pagination.Item>
+              ))}
+            <Pagination.Ellipsis />
+            <Pagination.Next />
+            <Pagination.Last />
+          </Pagination>
         </Container>
-        <Pagination>{pages}</Pagination>
       </Container>
     );
   }
